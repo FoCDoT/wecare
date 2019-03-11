5baab760b0a0b9a910d7bd5d12ca2e18b599bf8e
FoCDoT <egayakwad11@gmail.com>
2019-03-11 08:07:10 +0530

    intial view added

--- wecare/hackgrid/views.py
+++ wecare/hackgrid/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+import json
+from django.http import HttpResponse
 # Create your views here.
+
+def base_demo(response):
+    return HttpResponse("Data received")
+
+
